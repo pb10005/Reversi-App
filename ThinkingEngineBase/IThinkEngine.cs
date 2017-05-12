ce1f8306a3b4c4e27b37aa22e3b1b1168cf0cb6d
Kohei <atihsa.mihs.ne.ho.da@gmail.com>
2017-05-12 15:28:15 +0900

    思考エンジンのインターフェイスを追加

---
+++ ThinkingEngineBase/IThinkEngine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThinkingEngineBase
+{
+    /// <summary>
+    /// 思考エンジンのインターフェイス
+    /// </summary>
+    public interface IThinkEngine
+    {
+        /// <summary>
+        /// 盤の情報をもとに思考し、次の手を表す符号なし64ビット整数を返す
+        /// </summary>
+        /// <param name="black"></param>
+        /// <param name="white"></param>
+        /// <returns></returns>
+        ulong Think(ulong black,ulong white);
+        /// <summary>
+        /// 思考時間の上限を設定する
+        /// </summary>
+        /// <param name="milliSecond">単位: ミリ秒</param>
+        void SetTimeLimit(int milliSecond);
+    }
+}
